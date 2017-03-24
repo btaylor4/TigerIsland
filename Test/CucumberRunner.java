1c99718acb7f8c0d11dc19595babd4bb3981e5e0
btaylor4 <clearly.b.t@gmail.com>
2017-03-24 10:37:36 -0400

    Remvoing .idea files in github

---
+++ Test/CucumberRunner.java
@@ -0,0 +1,15 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by jdavi on 3/17/17.
+ */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = {"pretty","html:reports/test-report"},
+        tags = "@CreationTest"
+)
+
+public class CucumberRunner {
+}
