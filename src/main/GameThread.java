f7060b43b2c5fa3cdbc7018089332b344fe10225
Adam Soliman <solimada41@gmail.com>
2017-04-06 19:46:38 -0400

    added constructor and partial run method to GameThread class

--- src/main/GameThread.java
+++ src/main/GameThread.java
@@ -8,40 +8,25 @@ import java.util.ArrayList;
  */
 public class GameThread implements Runnable{
 
+    GameBoard game;
+    Player player1;
+    Player player2;
+    Player currentPlayer;
+
+    public GameThread(){
+        game = new GameBoard();
+        player1 = new Player(game,1);
+        player2 = new Player(game,2);
+    }
+
     @Override
     public void run() {
-        boolean game_on = false;
 
-        GameBoard game = new GameBoard();
-        Player player1 = new Player(game, 1);
-        Player player2 = new Player(game, 2);
-        Player currentPlayer ;
-
-        player1.playFirstTile();
-        game.printBoard();
-
-        currentPlayer = player2 ;
-
-        ArrayList<Integer> myList = new ArrayList<>();
-
-        System.out.println("Im so big" + myList.size());
-
-        myList.add(22);
-
-        System.out.println("Im so big" + myList.size());
-
-        myList.add(33);
-
-        myList.remove(0);
-
-        System.out.println(myList.get(0));
-
-
-        while (game_on) {
+        //server will tell us when game is over
+        while (true) {
             System.out.println("Player" + currentPlayer.designator + "'s turn");
             currentPlayer.playTilePhase();
             currentPlayer.build();
-            game_on = !currentPlayer.isOutOfPieces();
 
             if(currentPlayer.designator == 1)
                 currentPlayer = player2 ;
