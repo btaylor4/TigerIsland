c597e81a9b262d5d395ec5a6c18def05e362b337
Adam Soliman <solimada41@gmail.com>
2017-04-09 15:05:54 -0400

    added net folder to src

---
+++ src/net/NetServerMsg.java
@@ -0,0 +1,164 @@
+package net;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class NetServerMsg {
+
+    NetScanner scanner;
+    ArrayList<Token> tokens ;
+    public void ParseLine(String line)
+    {
+        scanner = new NetScanner();
+        tokens = new ArrayList<Token>() ;
+        scanner.SetBuffer(line);
+        Token t;
+
+        while(scanner.hasNext())
+        {
+            t = scanner.Scan();
+            tokens.add(t);
+        }
+    }
+    public String GetPlayerId()
+    {
+        Token token = GetTokenByType(TokenType.TOKEN_PLAYER);
+        if(token != null)
+            return (String)token.Data;
+        else {
+            token = GetTokenByType(TokenType.TOKEN_BEGIN);
+            if (token != null)
+                return (String) token.Data;
+        }
+        return null;
+    }
+    public int GetMoveId()
+    {
+        Token token = GetTokenByType(TokenType.TOKEN_MOVE);
+        if(token != null)
+            return (int) token.Data;
+        else
+            return -1;
+    }
+    public String GetGameId()
+    {
+        Token token = GetTokenByType(TokenType.TOKEN_GAME);
+        if(token != null)
+            return (String)token.Data;
+        else
+            return null;
+    }
+    public String GetChallengeId()
+    {
+        Token token = GetTokenByType(TokenType.TOKEN_CHALLENGE);
+        if(token != null)
+            return (String)token.Data;
+        else
+            return null;
+    }
+    public int GetNumMatchesToPlay()
+    {
+        return (int)GetTokenByType(TokenType.TOKEN_PLAY).Data;
+    }
+    public int GetRoundId()
+    {
+        return (int)GetTokenByType(TokenType.TOKEN_ROUND).Data;
+    }
+    public ArrayList<String> GetTile()
+    {
+        Token token = GetTokenByType(TokenType.TOKEN_PLACED);
+        if(token != null)
+            return (ArrayList<String>)token.Data;
+        else {
+            token = GetTokenByType(TokenType.TOKEN_PLACE);
+            if(token != null)
+                return (ArrayList<String>)token.Data;
+            return null;
+        }
+    }
+    public String GetMessage()
+    {
+        Token token = GetTokenByType(TokenType.TOKEN_LOST);
+        if(token != null)
+            return (String)token.Data;
+        else {
+            token = GetTokenByType(TokenType.TOKEN_FORFEITED);
+            if(token != null)
+                return (String)token.Data;
+            return null;
+        }
+    }
+    public String GetSettlementName()
+    {
+        Token token = GetTokenByType(TokenType.TOKEN_BUILT);
+        if(token != null)
+            return (String)token.Data;
+        else
+            return null;
+    }
+    public TileVector GetTitlePlacement()
+    {
+        Token token = GetTokenByTypeAndIndex(TokenType.TOKEN_AT, 1);
+        if(token != null)
+            return (TileVector)token.Data;
+        else
+            return null;
+    }
+    public TileVector GetBuildLocation()
+    {
+        Token token = GetTokenByTypeAndIndex(TokenType.TOKEN_AT, 2);
+        if(token != null)
+            return (TileVector)token.Data;
+        else
+            return null;
+    }
+    //BEGIN ROUND i of ROUNDS
+    public int GetTotalRounds()
+    {
+        Token token = GetTokenByType(TokenType.TOKEN_OF);
+        if(token != null)
+            return (int)token.Data;
+        else
+            return -1;
+    }
+    public float GetMoveTimeLimit()
+    {
+        Token token = GetTokenByType(TokenType.TOKEN_WITHIN);
+        if(token != null)
+            return (float)token.Data;
+        else
+            return -1;
+    }
+    // returns <Player_ID, SCORE>
+    public HashMap<String, Integer> GetGameResults()
+    {
+        Token token = GetTokenByType(TokenType.TOKEN_OVER);
+        if(token != null)
+            return (HashMap<String, Integer>)token.Data;
+        else
+            return null;
+    }
+    private Token GetTokenByType(TokenType type)
+    {
+        for ( Token token : tokens)
+        {
+            if(token.Type == type && token.Type != TokenType.TOKEN_EOS)
+            {
+                return token;
+            }
+        }
+        return null;
+    }
+
+    private Token GetTokenByTypeAndIndex(TokenType type, int index)
+    {
+        int i = 1;
+        for ( Token token : tokens)
+        {
+            if(token.Type == type && (i++ == index))
+            {
+                return token;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/net/TileVector.java
@@ -0,0 +1,53 @@
+package net;
+
+
+public class TileVector {
+    private int x;
+    private int y;
+    private int z;
+    private int orientation;
+    private String terrain;
+    public TileVector(int X, int Y, int Z, int Orientation)
+    {
+        x = X;
+        y = Y;
+        z = Z;
+        orientation = Orientation;
+    }
+    public TileVector(int X, int Y, int Z, String Terrain)
+    {
+        x = X;
+        y = Y;
+        z = Z;
+        orientation = -1;
+        terrain = Terrain;
+    }
+    public TileVector(int X, int Y, int Z)
+    {
+        x = X;
+        y = Y;
+        z = Z;
+        orientation = -1;
+    }
+    public int GetX(){ return x;};
+    public int GetY(){ return y;};
+    public int GetZ(){ return z;};
+
+    public void SetX(int X)
+    {
+        x = X;
+    }
+
+    public void SetY(int Y)
+    {
+        y = Y;
+    }
+    public void SetZ(int Z)
+    {
+        z = Z;
+    }
+    public int GetOrientation() {return orientation;}
+    public void SetOrientation(int Orientation)  { orientation = Orientation;}
+    public String GetTerrain(){ return terrain;}
+    public void SetTerrain(String Terrain)  { terrain = Terrain;}
+}
