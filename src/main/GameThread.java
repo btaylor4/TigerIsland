e3bfb8d87df262656890125a63c6660ea23b3c73
Adam Soliman <solimada41@gmail.com>
2017-04-10 01:49:27 -0400

    can replicate opponents moves on gameboard

--- src/main/GameThread.java
+++ src/main/GameThread.java
@@ -1,7 +1,14 @@
 package main;
 
+import main.enums.BuildOptions;
+import main.enums.TerrainType;
 import main.players.BryanAI;
+import main.utils.XYZ;
 import net.*;
+
+import static main.utils.constants.COLUMN_ADDS;
+import static main.utils.constants.ROW_ADDS;
+import static main.utils.constants.SIDES_IN_HEX;
 
 public class GameThread implements Runnable{
 
@@ -41,6 +48,7 @@ public class GameThread implements Runnable{
 
 
             if(isMyTurn){
+                //AI stuff goes here basically AI main method goes here:
                 AI.determineTilePlacementByAI();
                 AI.determineBuildByAI();
             }
@@ -54,7 +62,7 @@ public class GameThread implements Runnable{
                     }
                 }
                 //simulate opponents move
-                client.GetCurrentMessage();
+                replicateOpponentMove();
 
             }
 
@@ -64,15 +72,76 @@ public class GameThread implements Runnable{
 
         }
     }
+    private void replicateOpponentMove(){
+        BuildOptions buildOption;
 
-    //these will change once client is visible
-    public void applyOpponentsMoves(){
-        Opponent.playTilePhase();
-        Opponent.playBuildPhase();
-    }
+        Tile tile;
+        TerrainType A;
+        TerrainType B;
 
-    public void gameIsOver(){
-        gameOver = true;
+        TileVector opponentPlacement;
+
+        XYZ xyzTo2DConverter;
+        Point twoDimensionalPoint;
+
+        NetServerMsg opponentsMove = client.GetCurrentMessage();
+
+        //parse tile placement
+        tile = new Tile();
+        A = opponentsMove.GetTileTerrains().get(0);
+        B = opponentsMove.GetTileTerrains().get(1);
+        tile.assignTerrain(A,B);
+
+        opponentPlacement = opponentsMove.GetTitlePlacement();
+        tile.setRotation(opponentPlacement.GetOrientation());
+
+        xyzTo2DConverter = new XYZ(opponentPlacement.GetX(),opponentPlacement.GetY(),opponentPlacement.GetZ());
+        twoDimensionalPoint = xyzTo2DConverter.get2DTranslation();
+
+        //place the tile
+        game.setTile(tile,new ProjectionPack(twoDimensionalPoint));
+
+        //parse build action
+        opponentPlacement = opponentsMove.GetBuildLocation();
+        xyzTo2DConverter = new XYZ(opponentPlacement.GetX(),opponentPlacement.GetY(),opponentPlacement.GetZ());
+        twoDimensionalPoint = xyzTo2DConverter.get2DTranslation();
+
+        switch (opponentsMove.GetAction()){
+
+            case NONE:
+                break;
+            case EXPANDED:
+                A = opponentPlacement.GetTerrainType();
+                GameBoard.board[twoDimensionalPoint.row][twoDimensionalPoint.column].settlementPointer.expand(A);
+                break;
+            case FOUNDED:
+                buildOption = BuildOptions.FOUND_SETTLEMENT;
+                Settlement foundMe = new Settlement(game);
+                foundMe.owner = Opponent;
+                foundMe.beginNewSettlement(twoDimensionalPoint);
+                game.setSettlement(twoDimensionalPoint,foundMe);
+                Opponent.placeMeeple(twoDimensionalPoint,foundMe);
+                break;
+            case BUILT:
+                buildOption = opponentsMove.GetSettlement();
+                Settlement addTotoroOrTigerToMe = null;
+                for (int j=0; j<SIDES_IN_HEX; j++){
+                    if (GameBoard.board[twoDimensionalPoint.row+ROW_ADDS[j]][twoDimensionalPoint.column+COLUMN_ADDS[j]].settlementPointer.owner == Opponent){
+                        addTotoroOrTigerToMe = GameBoard.board[twoDimensionalPoint.row+ROW_ADDS[j]][twoDimensionalPoint.column+COLUMN_ADDS[j]].settlementPointer;
+                    }
+                }
+
+                if(buildOption == BuildOptions.TIGER_PLAYGROUND){
+                    Opponent.placeTiger(twoDimensionalPoint,addTotoroOrTigerToMe);
+                } else if (buildOption == BuildOptions.TOTORO_SANCTUARY) {
+                    Opponent.placeTotoro(twoDimensionalPoint,addTotoroOrTigerToMe);
+                }
+
+                break;
+        }
+
+
+
     }
 
 }
