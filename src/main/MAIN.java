5d0026165b366004f971b0740a3ec6c1cfc31603
Adam Soliman <solimada41@gmail.com>
2017-04-16 20:17:29 -0400

    deleted contents of else f statement when receiving forfeit or lost message

--- src/main/MAIN.java
+++ src/main/MAIN.java
@@ -74,8 +74,7 @@ public class MAIN
 
                             else if(client.message.contains("FORFEITED") || client.message.contains("LOST"))
                             {
-                                client.getNextMessageFromServer();
-                                continue;
+
                             }
 
                             else if(client.message.contains("GAME OVER"))
